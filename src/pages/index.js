263887149c521a49bd00ba4405771699e32198c3
SyedHussainSaber <syedhussainsaber8@gmail.com>
2023-06-24 00:44:11 +0530

    files added

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
   <About/>
   <div className="location">
     <h2 className="subHeading">Location</h2>
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3805.036269284354!2d78.4631603!3d17.505782!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb900fb5b9a82f%3A0x35d09c7a1a275241!2sSt.%20Moses%20High%20School!5e0!3m2!1sen!2sin!4v1687030094788!5m2!1sen!2sin" width="600" className='location-map' height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3805.036269284354!2d78.4631603!3d17.505782!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb900fb5b9a82f%3A0x35d09c7a1a275241!2sSt.%20Moses%20High%20School!5e0!3m2!1sen!2sin!4v1687030094788!5m2!1sen!2sin" width="600" className='location-map' height="450" style={{border:"0"}} loading="lazy"></iframe>
 
 <motion.div   
         initial={{ opacity: 0,translateX:150 }}
